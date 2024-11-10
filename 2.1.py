825c169a7cde4178c452cb7b682b63c2d2f1bdf8
TRENCIUSZ <92546130+Trenciusz@users.noreply.github.com>
2024-11-10 17:33:44 +0100

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,16 @@
+print("Írjon be kis 'a' betűvel kezdődő szavakt!")
+print("Ha nem kis 'a' betűvel kezdődő szót ír be, az nem lesz tárolva!")
+print("A végén ki fogom írni az összes tárolt szót!")
+
+szavak = []
+
+while True:
+    szo = input("Írjon be egy szót: ")
+    if szo == "":
+        break
+    if szo[0].lower() == "a":
+        szavak.append(szo)
+
+print("Az eltárolt szavak: ")
+for szo in szavak:
+    print(szo)
